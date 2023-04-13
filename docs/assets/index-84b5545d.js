b554172dfa84ca552f6bab7a170b355d83882b4d
Avi Deep <deepa814@newschool.edu>
2023-04-13 00:03:45 -0400

    WIP

--- docs/assets/index-84b5545d.js
+++ docs/assets/index-84b5545d.js
@@ -380,7 +380,6 @@ reactJsxRuntime_production_min.jsxs = q;
   }
 })(jsxRuntime);
 const jsx = jsxRuntimeExports.jsx;
-const jsxs = jsxRuntimeExports.jsxs;
 var client = {};
 var reactDomExports = {};
 var reactDom = {
@@ -7622,30 +7621,9 @@ var m = reactDomExports;
   client.createRoot = m.createRoot;
   client.hydrateRoot = m.hydrateRoot;
 }
-const reactLogo = "/elasticcollection/assets/react-35ef61ed.svg";
-const viteLogo = "/elasticcollection/vite.svg";
 const App$1 = "";
 function App() {
-  const [count, setCount] = reactExports.useState(0);
-  return /* @__PURE__ */ jsxs("div", { className: "App", children: [
-    /* @__PURE__ */ jsxs("div", { children: [
-      /* @__PURE__ */ jsx("a", { href: "https://vitejs.dev", target: "_blank", children: /* @__PURE__ */ jsx("img", { src: viteLogo, className: "logo", alt: "Vite logo" }) }),
-      /* @__PURE__ */ jsx("a", { href: "https://reactjs.org", target: "_blank", children: /* @__PURE__ */ jsx("img", { src: reactLogo, className: "logo react", alt: "React logo" }) })
-    ] }),
-    /* @__PURE__ */ jsx("h1", { children: "Vite + React" }),
-    /* @__PURE__ */ jsxs("div", { className: "card", children: [
-      /* @__PURE__ */ jsxs("button", { onClick: () => setCount((count2) => count2 + 1), children: [
-        "count is ",
-        count
-      ] }),
-      /* @__PURE__ */ jsxs("p", { children: [
-        "Edit ",
-        /* @__PURE__ */ jsx("code", { children: "src/App.jsx" }),
-        " and save to test HMR"
-      ] })
-    ] }),
-    /* @__PURE__ */ jsx("p", { className: "read-the-docs", children: "Click on the Vite and React logos to learn more" })
-  ] });
+  return /* @__PURE__ */ jsx("div", { className: "App" });
 }
 const index = "";
 client.createRoot(document.getElementById("root")).render(
--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import { useState } from "react";
-
-import "./App.css";
-import { allBooks } from "./data/books.js";
 
 function App() {
   return (
